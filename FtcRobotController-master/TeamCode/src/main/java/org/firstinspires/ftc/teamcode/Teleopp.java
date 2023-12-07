e873ea16ffaf37808c3e25183c38dfc2e5f27924
Anton2000_123 <anton2000_123@hotmail.com>
2023-12-06 18:10:53 -0600

    updates for Hang and acutator for intake coddd in. Replaced the Auto Arm back and forth for better usability.

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleopp.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleopp.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import static com.qualcomm.robotcore.hardware.DcMotor.ZeroPowerBehavior.BRAKE;
+import static java.lang.Thread.sleep;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -19,6 +20,15 @@ import com.qualcomm.robotcore.hardware.Gamepad;
         private ElapsedTime runtime = new ElapsedTime();
         private DcMotor LeftDrive = null;
         private DcMotor RightDrive = null;
+
+        private DcMotor Intake = null;
+        static final double     LEFT_CORRECTION         = 1.0;
+        static final double     RIGHT_CORRECTION        = 1.1;
+
+        private DcMotor AcutatorForIntake = null;
+
+        private Servo hangerServo = null;
+
         private Servo servo = null;
 
 
@@ -27,10 +37,12 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 
         @Override
         public void init() {
-
+            AcutatorForIntake = hardwareMap.get(DcMotor.class, "actuator");
+            Intake = hardwareMap.get(DcMotor.class, "intake");
             LeftDrive  = hardwareMap.get(DcMotor.class, "left");
             RightDrive = hardwareMap.get(DcMotor.class, "right");
             servo = hardwareMap.get(Servo.class, "servo");
+            hangerServo = hardwareMap.get(Servo.class, "hang");
             LeftDrive.setDirection(DcMotor.Direction.REVERSE);
             RightDrive.setDirection(DcMotor.Direction.FORWARD);
             LeftDrive.setZeroPowerBehavior(BRAKE);
@@ -64,22 +76,47 @@ import com.qualcomm.robotcore.hardware.Gamepad;
             double leftPower;
             double rightPower;
             double Turbo;
-            if (gamepad1.right_bumper) {
+            if (gamepad1.right_trigger >  0.5) {
                 Turbo = 1;
             } else {
                 Turbo = 0.6;
             }
 
+            if(gamepad1.left_trigger > 0.5){
+                if(gamepad1.x){
+                    hangerServo.setPosition(1);
+                }
+                else {
+                    hangerServo.setPosition(0);
+                }
+
+            }
             double drive = gamepad1.left_stick_y * Turbo;
             double turn  =  -gamepad1.right_stick_x * Turbo;
-            leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-            rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+            leftPower    = Range.clip(drive + turn, -1.0, 1.0) * LEFT_CORRECTION;
+            rightPower   = Range.clip(drive - turn, -1.0, 1.0) * RIGHT_CORRECTION;
 
             if (gamepad1.right_bumper) {
-                servo.setPosition(1);
+                //servo.setPosition(1);
+                Intake.setPower(1);
+
             } else if (gamepad1.left_bumper) {
-                servo.setPosition(0);
+                //servo.setPosition(0);
+                Intake.setPower(-1);
+            } else {
+                Intake.setPower(0);
             }
+
+            if(gamepad1.a){
+                AcutatorForIntake.setPower(1);
+            } else if(gamepad1.b){
+                AcutatorForIntake.setPower(-1);
+
+            }
+            else {
+                AcutatorForIntake.setPower(0);
+            }
+
 
 
             // Send calculated power to wheels
